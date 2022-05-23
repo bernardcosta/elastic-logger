70a682911a48a12abd79f8ebaea3ddd97c84ec72
Bernard Costa <costa.ber95@gmail.com>
2022-05-23 11:06:32 +0200

    Changed default port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser')
 const winston = require('winston')
 const expressWinston = require('express-winston');
 const app = express()
-const port = 3001
+const port = 3000
 
 app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
