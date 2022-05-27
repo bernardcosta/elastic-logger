0dd6e964b5e0a82462671236f0b0fc4803cc04c9
Bernard Costa <costa.ber95@gmail.com>
2022-05-27 11:59:37 +0200

    Updates

--- assets/api-guide.html.c899e6a1.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-7c22cc29",path:"/api-guide.html",title:"API Reference",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1653392236e3,contributors:[{name:"Bernard Costa",email:"costa.ber95@gmail.com",commits:1}]},filePathRelative:"api-guide.md"};export{e as data};
